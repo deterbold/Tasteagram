7455cf445ad45647e2442548a9d04dbe40daa60d
Miguel Sicart <deterbold@gmail.com>
2021-12-02 23:27:16 +0100

    to do: share function

--- Tasteagram/ImageController.swift
+++ Tasteagram/ImageController.swift
@@ -6,27 +6,141 @@
 //
 
 import UIKit
+import AVKit
+import CoreML
+import Lumina
+import Vision
 
-class ImageController: UIViewController {
+class ImageController: UIViewController
+{
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var image: UIImage?
+    var livePhotoURL: URL?
+    var showingDepth: Bool = false
+    var position: CameraPosition = .back
+    private var _depthData: Any?
+    
+    
+    private lazy var classificationRequest: VNCoreMLRequest =
+    {
+      do {
+        // 2
+        let model = try VNCoreMLModel(for: prototype_taste().model)
+        // 3
+        let request = VNCoreMLRequest(model: model) { request, _ in
+            if let classifications =
+              request.results as? [VNClassificationObservation] {
+              print("Classification results: \(classifications)")
+                print(classifications.first!.identifier as String as Any)
+                print(request.results?.first?.confidence as Any)
+                let veredict = classifications.first!.identifier as String
+                if veredict == "Good"
+                {
+                    print("good picture")
+                    self.sendPicture()
+                    
+                }
+                else if veredict == "Bad"
+                {
+                    print("bad picture")
+                    self.sendText()
+                }
+                
+            }
+        }
+        // 4
+        request.imageCropAndScaleOption = .centerCrop
+        return request
+      } catch {
+        // 5
+        fatalError("Failed to load Vision ML model: \(error)")
+      }
+    }()
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        self.imageView.image = image
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func judgePicture(_ sender: Any)
+    @IBAction func regretButton(_ sender: Any)
     {
+        //performSegue(withIdentifier: "regretAndReturn", sender: nil)
+        sendRegretText()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func judgePicture(_ sender: Any)
+    {
+        classifyImage(image!)
     }
-    */
+    
+    func classifyImage(_ image: UIImage) {
+      // 1
+      guard let orientation = CGImagePropertyOrientation(
+        rawValue: UInt32(image.imageOrientation.rawValue)) else {
+        return
+      }
+      guard let ciImage = CIImage(image: image) else {
+        fatalError("Unable to create \(CIImage.self) from \(image).")
+      }
+      // 2
+      DispatchQueue.global(qos: .userInitiated).async {
+        let handler =
+          VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
+        do {
+          try handler.perform([self.classificationRequest])
+        } catch {
+          print("Failed to perform classification.\n\(error.localizedDescription)")
+        }
+      }
+    }
+    
+    func sendPicture()
+    {
+        DispatchQueue.main.async {
+            self.performSegue(withIdentifier: "sucessControllerSegue", sender: self)
 
+        }
+    }
+    
+    func sendText()
+    {
+        DispatchQueue.main.async {
+            self.performSegue(withIdentifier: "failureSegue", sender: self)
+        }
+    }
+    
+    func sendRegretText()
+    {
+        DispatchQueue.main.async {
+            self.performSegue(withIdentifier: "regretAndReturn", sender: self)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.identifier == "sucessControllerSegue" {
+            let dvc = segue.destination as! SuccessController
+            dvc.image = self.image
+        }
+        
+        if segue.identifier == "failureSegue" {
+            let dvc = segue.destination as! IntroController
+            dvc.labelText = "That was Not a Good Picture. Try Again"
+        }
+        
+        if segue.identifier == "regretAndReturn"
+        {
+            let dvc = segue.destination as! IntroController
+            dvc.labelText = "let the AI decide"
+        }
+    }
 }
+
--- Tasteagram/IntroController.swift
+++ Tasteagram/IntroController.swift
@@ -6,15 +6,27 @@
 //
 
 import UIKit
+import AVKit
 import Lumina
 
 class IntroController: UIViewController
 {
-
+    
+    @IBOutlet weak var decision_label: UILabel!
+    
+    var labelText = "let the AI decide"
+    
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        decision_label.font = UIFont(name: "Futura Medium", size: 28)
+        decision_label.adjustsFontSizeToFitWidth = true
+        decision_label.allowsDefaultTighteningForTruncation = true
+        decision_label.numberOfLines = 0
+        decision_label.textAlignment = .center
+        decision_label.text = labelText
     }
     
 
@@ -23,12 +35,103 @@ class IntroController: UIViewController
         //code for setting up the camera and presenting the view goes here.
         //creating the camera view controller
         let camera = LuminaViewController()
+        camera.delegate = self
         
         //setup for the camera goes here
         
         //presenting the camera view
+        camera.modalPresentationStyle = .fullScreen
         present(camera, animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+      if segue.identifier == "showImageController"{
+        guard let controller = segue.destination as? ImageController else { return }
+        if let map = sender as? [String: Any] {
+          controller.image = map["stillImage"] as? UIImage
+          controller.livePhotoURL = map["livePhotoURL"] as? URL
+          guard let positionBool = map["isPhotoSelfie"] as? Bool else { return }
+          controller.position = positionBool ? .front : .back
+        } else { return }
+      }
     }
     
 }
 
+extension IntroController: LuminaDelegate
+{
+    func captured(stillImage: UIImage, livePhotoAt: URL?, depthData: Any?, from controller: LuminaViewController) {
+        controller.dismiss(animated: true) {
+            self.performSegue(withIdentifier: "showImageController", sender: ["stillImage": stillImage, "livePhotoURL": livePhotoAt as Any, "depthData": depthData as Any, "isPhotoSelfie": controller.position == .front ? true : false])
+        }
+    }
+
+    
+//    func captured(stillImage: UIImage, livePhotoAt: URL?, depthData: Any?, from controller: LuminaViewController){
+//      controller.dismiss(animated: true) {
+//        self.performSegue(withIdentifier: "stillImageOutputSegue", sender: ["stillImage": stillImage, "livePhotoURL": livePhotoAt as Any, "depthData": depthData as Any, "isPhotoSelfie": controller.position == .front ? true : false])
+//      }
+//    }
+//
+      //This is for capturing video. Not needed in the first version
+//    func captured(videoAt: URL, from controller: LuminaViewController) {
+//        <#code#>
+//    }
+//
+//    func streamed(videoFrame: UIImage, from controller: LuminaViewController) {
+//        <#code#>
+//    }
+//
+//    func streamed(videoFrame: UIImage, with predictions: [LuminaRecognitionResult]?, from controller: LuminaViewController) {
+//        <#code#>
+//    }
+//
+//    func streamed(depthData: Any, from controller: LuminaViewController) {
+//        <#code#>
+//    }
+//
+    func detected(metadata: [Any], from controller: LuminaViewController) {
+        print(metadata)
+    }
+    
+    func dismissed(controller: LuminaViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+//    func tapped(at: CGPoint, from controller: LuminaViewController) {
+//        <#code#>
+//    }
+    
+}
+
+extension CVPixelBuffer
+{
+  func normalizedImage(with position: CameraPosition) -> UIImage? {
+    let ciImage = CIImage(cvPixelBuffer: self)
+    let context = CIContext(options: nil)
+    if let cgImage = context.createCGImage(ciImage, from: CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(self), height: CVPixelBufferGetHeight(self))) {
+      return UIImage(cgImage: cgImage, scale: 1.0, orientation: getImageOrientation(with: position))
+    } else {
+      return nil
+    }
+  }
+
+  private func getImageOrientation(with position: CameraPosition) -> UIImage.Orientation {
+    let orientation = UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.windowScene?.interfaceOrientation ?? .portrait
+    switch orientation {
+      case .landscapeLeft:
+        return position == .back ? .down : .upMirrored
+      case .landscapeRight:
+        return position == .back ? .up : .downMirrored
+      case .portraitUpsideDown:
+        return position == .back ? .left : .rightMirrored
+      case .portrait:
+        return position == .back ? .right : .leftMirrored
+      case .unknown:
+        return position == .back ? .right : .leftMirrored
+      @unknown default:
+        return position == .back ? .right : .leftMirrored
+    }
+  }
+}
